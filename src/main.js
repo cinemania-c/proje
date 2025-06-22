01becbbbb69ff838b59a31bcdccb7bb3d2ba399d
sahinbilisim <shnblsm@gmail.com>
2025-06-22 16:53:18 +0300

    main-

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
+// Import styles
 import './js/header';
 import './js/theme';
 import './js/fetchMovies';
 import './js/hero';
 import './js/footer';
-import './js/scroll-to-top'
-import './js/modal'
+import './js/scroll-to-top';
+import './js/modal';
 import './js/pagination';
-
-
 
 import './js/weekly';
 import './js/library';
